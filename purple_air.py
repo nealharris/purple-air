08727dcd765ce4c21b4efc3eb3ec7708a95ed6ae
Neal Harris <neal.harris@gmail.com>
2018-12-08 15:02:06 -0800

    working lambda

--- purple_air.py
+++ purple_air.py
@@ -1,9 +1,20 @@
+import os
+from datetime import datetime
+from urllib.request import Request, urlopen
 import json
-from urllib.request import urlopen
 from enum import Enum
-import sys
+import boto3
 
 PURPLE_AIR_URL_PREFIX = 'https://www.purpleair.com/json?show='
+
+SENSOR_ID = os.environ['sensor_id']
+BUCKET_NAME = os.environ['bucket_name']
+FILENAME = os.environ['filename']
+TOPIC_ARN = os.environ['topic_arn']
+MIN_COLOR_NOTIF_THRESHOLD = int(os.environ['min_color_notif_threshold'])
+
+s3 = boto3.client('s3')
+sns = boto3.client('sns')
 
 class Color(Enum):
     green        = 1
@@ -14,16 +25,9 @@ class Color(Enum):
     maroon       = 6
     hella_maroon = 7
 
-def pm_2_5_average(sensor_idi):
-    data = json.load(urlopen(PURPLE_AIR_URL_PREFIX + str(sensor_id)))
-    
-    stats0 = json.loads(data["results"][0]["Stats"])
-    stats1 = json.loads(data["results"][1]["Stats"])
-
-    return (stats0["v"] + stats1["v"])/2.
 
 # Source: https://aqicn.org/faq/2013-09-09/revised-pm25-aqi-breakpoints/
-def aqi_category(pm_2_5_value):
+def current_color(pm_2_5_value):
     if pm_2_5_value <= 12.0:
         return Color.green
     elif pm_2_5_value <= 35.4:
@@ -40,8 +44,69 @@ def aqi_category(pm_2_5_value):
         return Color.hella_maroon #shit
 
 
-if __name__ == "__main__":
-    sensor_id = sys.argv[1]
-    pm25 = pm_2_5_average(sensor_id)
-    category = aqi_category(pm25)
-    print(category.name)
+def get_sensor_data():
+    print("Attempting to open "+ PURPLE_AIR_URL_PREFIX + str(SENSOR_ID))
+    return json.load(urlopen(PURPLE_AIR_URL_PREFIX + str(SENSOR_ID)))
+
+
+def pm_2_5_average(data):
+    stats0 = json.loads(data["results"][0]["Stats"])
+    stats1 = json.loads(data["results"][1]["Stats"])
+
+    return (stats0["v"] + stats1["v"])/2.
+
+
+def get_last_color():
+    response = s3.get_object(Bucket=BUCKET_NAME, Key=FILENAME)
+    color_name = response['Body'].read().decode('utf-8').strip()
+    return Color[color_name]
+
+
+def update_color(color):
+    s3.put_object(Bucket=BUCKET_NAME, Key=FILENAME, Body=color.name)
+
+
+def notify_color_change(old_color, new_color):
+    if old_color.value < new_color.value:
+        print("degraded")
+        message = "The air has degraded from " + old_color.name + " to " + new_color.name
+    else:
+        print("improved")
+        message = "The air has improved from " + old_color.name + " to " + new_color.name
+    print("About to send notification of change")
+    sns.publish(TopicArn=TOPIC_ARN,Message=message)
+
+
+def should_notify_color_change(old_color, new_color):
+    return max(old_color.value, new_color.value) >= MIN_COLOR_NOTIF_THRESHOLD
+
+
+def lambda_handler(event, context):
+    print('Checking on sensor {} at {}...'.format(SENSOR_ID, event['time']))
+    try:
+        current_data = get_sensor_data()
+        current_pm_2_5 = pm_2_5_average(current_data)
+
+        print("current pm2.5 reading is: " +  str(current_pm_2_5))
+
+        new_color = current_color(current_pm_2_5)
+        last_color = get_last_color()
+
+        if not new_color == last_color:
+            print("new color!")
+            update_color(new_color)
+            if should_notify_color_change(last_color, new_color):
+                print("sending color change notification")
+                notify_color_change(last_color, new_color)
+            else:
+                print("change below notification threshold")
+        else:
+            print("no change in color")
+    except:
+        print('Check failed!')
+        raise
+    else:
+        print('Check succeeded!')
+        return event['time']
+    finally:
+        print('Check complete at {}'.format(str(datetime.now())))
